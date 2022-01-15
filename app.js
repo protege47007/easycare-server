2c5e8e58e4d5b5b9e77133a3364a3b5f087cc875
protege47007 <davidak.a@yahoo.com>
2022-01-15 05:51:39 +0100

    complete cloudinary integration

--- app.js
+++ app.js
@@ -5,24 +5,28 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 const express = require("express");
 const encrypt = require("mongoose-encryption");
-const cloudinary = require('cloudinary');
+const cloudinary = require('cloudinary').v2;
 const mailer = require('nodemailer');
 const session = require("express-session")
 const passport = require("passport");
-const pLMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
-
+cloudinary.config({
+  cloud_name: 'easycare-ng',
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+})
 
 //`mongodb+srv://protege47007:${process.env.PASS}@cluster0.5nisq.mongodb.net/easycareDb`
-// CLOUDINARY_URL=`cloudinary://${process.env.CLOUDKEY}:${process.env.CLOUDSECRET}@easycare-ng`
+// CLOUDINARY_URL=`cloudinary://${process.env.CLOUD_KEY}:${process.env.CLOUD_SECRET}@easycare-ng`
 
 //calling express
 const app = express();
 
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(session({
   secret: process.env.KEY,
   resave: false,
@@ -32,8 +36,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-
-mongoose.connect('mongodb://localhost:27017/eascareDb', {
+// 'mongodb://localhost:27017/easycareDb'
+mongoose.connect(`mongodb+srv://protege47007:${process.env.PASS}@cluster0.5nisq.mongodb.net/easycareDb`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -65,11 +69,11 @@ const adminSchema = new mongoose.Schema({
 
 
 // users collection encryption
-userSchema.plugin(pLMongoose);
+userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-const Clients = mongoose.model("Client", clientSchema);
-const CGivers = mongoose.model("CareGiver", careGiverSchema);
+const Client = mongoose.model("Client", clientSchema);
+const CGiver = mongoose.model("CareGiver", careGiverSchema);
 const User = mongoose.model("User", userSchema);
 
 passport.serializeUser(function(user, done) {
@@ -84,7 +88,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3030/oauth20/google/",
+  callbackURL: "http://localhost:3030/auth/google/",
   // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
@@ -103,6 +107,8 @@ app
   .post((req,res) => {
 
   })
+
+
 
 //routes
 app
@@ -125,29 +131,23 @@ app
             if (err) {
               console.error(err, 'login error');
             } else{
-              passport.authenticate('local') (req, res, () => {
-                res.write('successfully logged in redirecting to profile page', 'utf8');
-                res.end();
-                // res.redirect('/profile');
+              passport.authenticate('local',{
+                failureRedirect: '/login',
+              }) (req, res, () => {
+                res.redirect('/profile')
               })
             }
           });
         } else {
-            res.write("Current password/email does not match*", "utf-8");
             res.redirect('/login');
         }
       }
     });
 });
 
-app.get("/login/google", (req, res) => {
-  passport.authenticate('google', { scope: ['profile'] });
-});
+app.get("/login/google", passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/oauth20/google/profile', 
-passport.authenticate('google', { failureRedirect: '/login' }),
-function(req, res) {
-  // Successful authentication, redirect home.
+app.get('/auth/google/', passport.authenticate('google', { failureRedirect: '/login'}), (req, res) => {
   res.redirect('/profile');
 });
 
@@ -155,13 +155,13 @@ app
   .route("/signup")
   
   .post((req, res) => {
-    User.findOne({ email: req.body.mail }, function (err, foundUser) {
+    User.findOne({ email: req.body.mail.toLowerCase() }, function (err, foundUser) {
       if (err) {
         console.log("user collection error:", err);
       } else { 
         if (foundUser) {
             
-            res.redirect('/login/already');
+            res.redirect('/login/already'); //already a member
         } else {
           User.register({username: req.body.mail.toLowerCase()}, req.body.password, (err, user) => {
             if (err) {
@@ -174,15 +174,12 @@ app
                   fullname: req.body.fullname,
                   email: req.body.mail,
                 }
-                let output = saveToDb(data, Clients);
+                let output = saveToDb(data, Client);
                 if (!output) {
                   res.redirect('/profile');
                 } else {
                   res.redirect('/signup');
                 }
-                // res.write('welcome to the family')
-                // res.json()
-                res.redirect('/profile');
               })
             }
           })
@@ -218,7 +215,7 @@ app
                   fullname: req.body.fullname,
                   email: req.body.mail,
                 }
-                let output = saveToDb(data, CGivers);
+                let output = saveToDb(data, CGiver);
                 if (!output) {
                   res.redirect('/profile');
                 } else {
@@ -250,7 +247,6 @@ app
 app.get('/profile', (req, res) => {
   if(req.isAuthenticated()){
     res.write('welcome to easy care!', 'utf8');
-    res.json({fullname: 'john doe', mail: 'john@doe.com'});
     res.end();
   } else {
     res.redirect('/login');
@@ -291,6 +287,56 @@ app.post("/contact", (req, res) => {
   }
 });
 
+app.get('/contact/posts', (req, res) => {
+  Contact.find({}, (err, posts) => {
+    res.send(posts);
+  })
+});
+
+const articleSchema = new mongoose.Schema({
+  date: String,
+  author: String,
+  article: String,
+  image: String,
+});
+
+const Article = mongoose.model("article", articleSchema);
+
+
+app.route('/news')
+  .post(async (req, res) => {
+    try {
+      await cloudinary.uploader.upload(
+        req.body.image,
+        function(error, result) { 
+          console.log(result);
+          const post = {
+            date: req.body.date,
+            author: req.body.author,
+            article: req.body.article,
+            image: result.secure_url,
+          }
+          let output = saveToDb(post, Article);
+          if (!output) {
+            res.status(200).redirect('/news')
+          }
+        }
+      );  
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({err: "something went wrong"});
+    }
+    
+  })
+
+  .get((req, res) => {
+    Article.find({}, (err, articles) => {
+      if(err) console.log('error getting news articles')
+      else{
+        res.send(articles);
+      }
+    })
+  });
 
 function saveToDb(obj, collectionName){
   const instance = new collectionName(obj);
@@ -312,7 +358,7 @@ const transporter = mailer.createTransport({
 });
 
 let mailOptions= {
-  from: 'youremail@gmail.com',
+  from: 'stalker@stalker.com',
   to: 'myFriend@gmail.com', // multiple reciepients 'a@mail.com, b@gamil.com '
   subject: 'Sending Email using Node.js',
   text: 'this was some what easy!.. ;)' // or html: '<h1>welcome</h1><p>that was easy!</p>'
