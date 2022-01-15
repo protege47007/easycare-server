d793ff6e3108e3a0f4c4264bd70c594b952e57a6
protege47007 <davidak.a@yahoo.com>
2022-01-15 20:12:50 +0100

    minor patch to add path module

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // 'mongodb://localhost:27017/easycareDb'
-mongoose.connect("mongodb://localhost:27017/easycareDb", {
+mongoose.connect(`mongodb+srv://protege47007:${process.env.PASS}@cluster0.5nisq.mongodb.net/easycareDb`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -310,7 +310,7 @@ app
       await cloudinary.uploader.upload(
         req.body.image,
         function (error, result) {
-          console.log(result);
+          // console.log(result);
           const post = {
             date: req.body.date,
             author: req.body.author,
@@ -340,6 +340,7 @@ app
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+const path = require('path');
 
 const DatauriParser = require('datauri/parser');
 const parser = new DatauriParser();
@@ -372,7 +373,7 @@ app.post("/news/test", upload.single("image"), async (req, res) => {
         }
       )
     }
-    
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ err: "something went wrong" });
